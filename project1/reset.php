b4fa63802050990beb0f0ff17f506ff02097f178
Omar <omarelmedny17@gmail.com>
2020-09-10 10:13:11 +0200

    project 1

---
+++ project1/reset.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Password Reset</title>
+	<link rel="stylesheet" href="main.css">
+</head>
+<body>
+	<form class="login-form" action="enter_email.php" method="post">
+		<h2 class="form-title">Reset password</h2>
+
+		<div class="form-group">
+			<label>Your email address</label>
+			<input type="email" name="email">
+		</div>
+		<div class="form-group">
+			<button type="submit" name="reset-password" class="login-btn">Submit</button>
+		</div>
+	</form>
+</body>
+</html>
