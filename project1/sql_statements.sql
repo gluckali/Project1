827e2a547ddb5cdc8ce4a5483582a553a401b935
Omar <omarelmedny17@gmail.com>
2020-09-24 10:54:01 +0200

    first commit

---
+++ project1/sql_statements.sql
@@ -0,0 +1,17 @@
+-- db aanmaken
+create database project1;
+
+  --tabel naam aanmaken / id not null primary key
+  Create Table tabelnaam;
+  id int not null auto_increment,
+--  primary key(id)
+
+  -- naam email account id id
+ naam varchar(250),
+ email varchar(250),
+ account_id int,
+ primary key(id),
+ --
+
+
+ -- persoon tabel 
